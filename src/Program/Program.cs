ad8ef9546464370d15d6f32c2ae15dead4e39351
Sibila <sibila.araujo@correo.ucu.edu.uy>
2023-04-26 20:34:40 -0300

    Creo un mago

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -6,6 +6,17 @@ namespace Program
     {
         static void Main(string[] args)
         {
+
+            Item item1 = new Item ("Bastón Mágico", 52, 0, "Arma");
+            Item item2 = new Item ("Capa Mágica", 52, 0, "Defensa");
+
+            Mago mago1 = new Mago ("Gerardo", 100);
+
+            mago1.AñadirItem(item1);
+            mago1.AñadirItem(item2);
+
+
+
         }
     }
 }
