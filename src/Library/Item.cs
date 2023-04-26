514db907d18ec4d9f7aacaa6f01514090456fee1
RodrigoCardinal <rodrigo.cardinal@correo.ucu.edu.uy>
2023-04-26 20:04:48 -0300

    Se agrego la clase Item

---
+++ src/Library/Item.cs
@@ -0,0 +1,17 @@
+namespace Program{
+    public class Item{
+
+        public string Nombre{get;set;}
+        public int Damage{get;set;}
+        public int Defence{get;set;}
+        public string Type{get;set;}
+        public Item (string nombre,int damage,int defence,string type){
+
+            this.Nombre = nombre;
+            this.Damage = damage;
+            this.Defence = defence;
+            this.Type = type;
+
+        }
+    }
+}
