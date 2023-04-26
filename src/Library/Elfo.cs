34aca09cf50f2357e0423d854b2f911cb4dcf5af
TadeoKi <tadeo.ki@correo.ucu.edu.uy>
2023-04-26 20:33:53 -0300

    Clase Elfo

---
+++ src/Library/Elfo.cs
@@ -0,0 +1,120 @@
+using System.Collections.Generic;
+
+namespace Program
+{
+    public class Elfo
+    {    
+        private string nombre;
+        private int vida;
+        private Dictionary<string, Item> items;
+
+        public int Vida
+        {
+            get
+            {
+                return this.vida;
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+                    this.vida = 0; 
+                }
+                else
+                {
+                    this.vida = value;
+                }
+            }
+        }
+        public string Nombre
+        {
+            get
+            {
+                return this.nombre;
+            }
+
+            set
+            {
+                this.nombre = value;
+            }
+        }
+        public Dictionary<string, Item> Items
+        {
+            get
+            {
+                return this.items;
+            }
+
+            set
+            {
+                this.items = value;
+            }
+        }
+
+        public Elfo (string nombre, int vida)
+        {
+            this.nombre = nombre;
+            this.vida = vida;
+            this.items = new Dictionary<string, Item>();
+        }
+
+        public void AtacarMago (Mago enemigo)
+        {       
+            if (enemigo.CalculoDefensa() < CalculoAtaque())
+            {
+                enemigo.Vida -= CalculoAtaque() - enemigo.CalculoDefensa();
+            }
+        }
+         public void AtacarElfo (Elfo enemigo)
+        {       
+            if (enemigo.CalculoDefensa() < CalculoAtaque())
+            {
+                enemigo.Vida -= CalculoAtaque() - enemigo.CalculoDefensa();
+            }
+        }
+         public void AtacarEnano (Enano enemigo)
+        {       
+            if (enemigo.CalculoDefensa() < CalculoAtaque())
+            {
+                enemigo.Vida -= CalculoAtaque() - enemigo.CalculoDefensa();
+            }
+        }
+
+        public void AñadirItem (Item item)
+        {
+            this.Items[item.Type] = item;
+        }
+
+        public void RemoverItem (Item item)
+        {
+            this.Items.Remove(item.Type);
+        }
+
+        public int CalculoDefensa ()
+        {
+            int defensaTotal = 0;
+            foreach (string item in Items.Keys)
+            {
+                if (item.Contains("defensa"))
+                {
+                    defensaTotal += Items[item].Defence;
+                }
+            }
+            return defensaTotal;
+        }
+
+        public int CalculoAtaque ()
+        {
+            int ataqueTotal = 0;
+            foreach (string item in Items.Keys)
+            {
+                if (item.Contains("arma"))
+                {
+                    ataqueTotal += Items[item].Damage;
+                }
+            }
+            return ataqueTotal;
+        }
+    }
+}
