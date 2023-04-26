9e5bcdeeab4727b5091d267164997b7025670909
Sibila <sibila.araujo@correo.ucu.edu.uy>
2023-04-26 20:54:30 -0300

    Un test

--- src/Test/Library.Test/ExampleTest.cs
+++ src/Test/Library.Test/ExampleTest.cs
@@ -1,13 +1,23 @@
 using NUnit.Framework;
 
-namespace Test.Library
+namespace Program
 {
     public class ExampleTest
     {
         [Test]
-        public void dummyTest()
+        public void RemoverItem()
         {
-            Assert.True(true);
+
+            Item item1 = new Item ("Bastón Mágico", 52, 0, "Arma");
+            Item item2 = new Item ("Capa Mágica", 52, 0, "Defensa");
+
+            Mago mago1 = new Mago ("Gerardo", 100);
+            mago1.RemoverItem(item1);
+
+            bool contiene = mago1.Items.ContainsValue(item1);
+            Assert.IsFalse(contiene);
+
+
         }
     }
 }
